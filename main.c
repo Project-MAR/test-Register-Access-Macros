406b76172856cbb34c14248adce44c5201ff683f
RAM <atitep.ram@gmail.com>
2017-02-26 22:22:45 +0700

    clean code

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 #define U32 unsigned long
 
 typedef struct WDT_REGS_TAG
@@ -11,13 +10,11 @@ typedef struct WDT_REGS_TAG
 	volatile U32 SR;
 }WDT_REGS;
 
-
 #define WDT    ((volatile WDT_REGS *) (&WDT_REGISTER))
 volatile U32 WDT_REGISTER[4] = {0x000001FF, 0xFFFFFFFF, 0x00001003, 0x40000000};
 
 int main(void)
 {
-
 	int i;
 	for (i=0; i<4; i++)
 		printf("Value in U32 WDT_REGISTER[i] : 0x%08x Address: 0x%08x\n",i ,WDT_REGISTER[i], &WDT_REGISTER[i]);
@@ -25,7 +22,6 @@ int main(void)
 	printf("CR: 0x%08x\n", WDT->CR);
 	printf("MR: 0x%08x\n", WDT->MR);
 	printf("SR: 0x%08x\n\n", WDT->SR);
-
 
 	WDT->CR = 0xA5000FF0;
 	WDT->MR = 0xFFF000FF;
@@ -43,4 +39,3 @@ int main(void)
 
 	return EXIT_SUCCESS;
 }
-
